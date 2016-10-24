df4183026b37f415acd6e6a52ddfed60484e6a19
ariadiprana <ari.adiprana@gmail.com>
2016-10-24 20:11:18 +0700

    release

--- models/dates.js
+++ models/dates.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose')
+const moment = require('moment');
 var Schema = mongoose.Schema;
 
 var dateSchema = mongoose.Schema({
@@ -6,5 +7,10 @@ var dateSchema = mongoose.Schema({
     freq:Number,
     dtCreated:Date
 })
+dateSchema.methods.toJSON = function() {
+    var obj = this.toObject()
+    obj.letter = moment(obj.letter).format('YYYY-MM-DD')
+    return obj
+};
 
 module.exports= mongoose.model('dates', dateSchema)
