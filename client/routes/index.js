cdd0e25aabb5a72cdcfe0718b45879a3466673aa
ivangerard <ivan.gerard@hotmail.com>
2016-10-24 13:53:10 +0700

    User Register Passport done

--- client/routes/index.js
+++ client/routes/index.js
@@ -4,12 +4,4 @@ var router = express.Router()
 
 //export router
 
-router.get('/', function(req, res, next) {
-    res.render('pages/home')
-})
-
-router.get('/register', function(req, res, next) {
-    res.render('pages/register')
-})
-
 module.exports = router
